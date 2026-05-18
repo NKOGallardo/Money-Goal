890e697c18b706ee31c7d1a2acf36fc75163af80
NKOGallardo <nkogalado@gmail.com>
2026-05-18 20:40:53 +0200

    got progess basics

--- Backend.js
+++ Backend.js
@@ -28,3 +28,23 @@ function ShowGoal() {
 updateBtn.addEventListener('click', () => {
     ShowGoal();
 });
+
+var i = 0;
+function move() {
+  if (i == 0) {
+    i = 1;
+    var elem = document.getElementById("myBar");
+    var width = 1;
+    var id = setInterval(frame, 1);
+    
+    function frame() {
+      if (width >= 100) {
+        clearInterval(id);
+        i = 0;
+      } else {
+        width++;
+        elem.style.width = width + "%";
+      }
+    }
+  }
+}
